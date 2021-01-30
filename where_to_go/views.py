f1ab8cc456a3fdc654fb2b69b58ff363f79fedcc
Luka Pacholli <luka319@i.ua>
2021-01-30 20:58:52 +0200

    Add blank start page

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,9 @@
 # views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def show_one(request):
     print('Кто-то зашёл на главную!')
-    return HttpResponse('<h1>Здесь будет карта</h1>')
+    return render(request, "one.html")
 
 
